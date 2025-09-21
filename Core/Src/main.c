0d9fd3b12ac2c9966b1d1557f636ae545e38b958
MichalBien <michalbien98@gmail.com>
2025-09-21 14:23:37 +0200

    star working

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -96,9 +96,9 @@ uint8_t Bugfix =11;
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  Bugfix = 33;
-	  Bugfix = 44;
-	  Bugfix = 55;
+	  Bugfix = 111;
+	  Bugfix = 222;
+	  Bugfix = 333;
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
