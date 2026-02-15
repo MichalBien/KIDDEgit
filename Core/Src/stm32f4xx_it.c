41afd2d7aec8318592838d0b34bc4facf2ac4ae9
MichalBien <michalbien98@gmail.com>
2026-02-15 10:21:10 +0100

    add f1

--- Core/Src/stm32f4xx_it.c
+++ Core/Src/stm32f4xx_it.c
@@ -41,7 +41,7 @@
 
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN PV */
-
+uint8_t f1;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
